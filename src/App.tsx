750062f174d0bed604f3fdebac43a01664df6c7d
Maytê <maytearaujo@yahoo.com.br>
2024-06-11 22:09:28 -0300

    fix: corrigindo erros

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import GlobalStyles from "./styles/global"
 import { Outlet } from "react-router-dom"
 
 import Root from "./components/routes/root"
-import Footer from "./components/Footer/Footer.jsx"
+import Footer from "./components/Footer/Footer"
 
 function App() {
 
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,4 @@
 
-import React from 'react'
 
 import Products from '../components/Products/Products'
 
